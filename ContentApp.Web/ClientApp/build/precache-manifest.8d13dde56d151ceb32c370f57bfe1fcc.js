ef4546bf0b6aea40bc4f3c0bdc6e2a39a34c7d80
Elnur Mammadli <elnoormobile@gmail.com>
2020-04-21 04:29:41 -0600

    build react app

---
+++ ContentApp.Web/ClientApp/build/precache-manifest.8d13dde56d151ceb32c370f57bfe1fcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "defab44e4fa16af55269ce86776025da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d976c433b82ab6d445ee",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "626427fbaa0bbc15861a",
+    "url": "/static/css/main.5d6784d6.chunk.css"
+  },
+  {
+    "revision": "d976c433b82ab6d445ee",
+    "url": "/static/js/2.2441dada.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2441dada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626427fbaa0bbc15861a",
+    "url": "/static/js/main.d3d88a80.chunk.js"
+  },
+  {
+    "revision": "c1ba5688b5285e4eb404",
+    "url": "/static/js/runtime-main.c94911a2.js"
+  }
+]);
